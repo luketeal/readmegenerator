4d4763fc62af7ce9fbac56f0966d0c47c9adb6c4
Luke Teal <l.teal21@gmail.com>
2021-05-06 11:37:02 -0400

    add code layout add generate markdown file

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // Include file system and inquirer packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { generateMarkdown } = require('./utils/generate-markdown.js')
+
+// Create an array of questions for user input
+
+// create a function to ask the questions
+
+// Create a function to write README file
+
+// Create a function to initialize app
+
+// Function call to initialize app
---
+++ utils/generate-markdown.js
@@ -0,0 +1,9 @@
+// Create a function to generate markdown for README
+function generateMarkdown(data) {
+    return `# ${data.title}
+  
+  `;
+  }
+  
+//   export generateMarkdown as a utility
+  module.exports = generateMarkdown;
