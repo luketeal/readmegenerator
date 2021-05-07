bfa6d2a50d6c7d939e91a753b95bdd16bdc15bad
Luke Teal <l.teal21@gmail.com>
2021-05-07 16:02:37 -0400

    Add markdown info to generate-markdown function

--- utils/generate-markdown.js
+++ utils/generate-markdown.js
@@ -1,8 +1,31 @@
 // Create a function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
-  
-  `;
+![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-blue.svg)
+## Description 
+${data.desciption}
+## Table of Contents 
+* [Description](#Description)  
+* [Installation](#Installation)  
+* [Usage](#Usage)  
+* [Contributing](#Contributing)  
+* [Tests](#Tests)  
+* [Questions](#Questions)  
+* [License](#License)  
+## Installation 
+${data.installation}
+## Usage 
+${data.usage}
+## License
+${data.license}
+## Contributing 
+${data.contribute}
+## Tests
+${data.test}
+## Questions
+If you have any questions, contact me:
+Email: ${data.email}
+GitHub Profile: ${data.github}`;
   }
   
 //   export generateMarkdown as a utility
