e64c99810fc7867cf4ae5ffcbc76d8dd8f899f7c
Luke Teal <l.teal21@gmail.com>
2021-05-07 14:40:05 -0400

    update questions, add init function to gen md

--- index.js
+++ index.js
@@ -6,48 +6,69 @@ const { generateMarkdown } = require('./utils/generate-markdown.js')
 // Create an array of questions for user input
 const questions = [
     {
+        type: 'input',
         name: 'title',
         message: 'What is the title of your application?',
     },
     {
+        type: 'input',
         name: 'description',
         message: 'Provide a description of your application.',
     },
     {
+        type: 'input',
         name: 'installation',
         message: 'Provide installation instructions for your application.',
     },
     {
+        type: 'input',
         name: 'usage',
         message: 'Provide usage instructions for your application.',
     },
     {
+        type: 'input',
         name: 'contribute',
         message: 'Provide contributing guidelines for your application.',
     },
     {
+        type: 'input',
         name: 'test',
         message: 'Provide test instructions for your application.',
     },
     {
+        type: 'choice',
         name: 'license',
         message: 'How is your application licensed?',
+        options: 'MIT'
     },
     {
+        type: 'input',
         name: 'email',
         message: 'What is your email address?',
     },
     {
+        type: 'input',
         name: 'github',
         message: 'What is your GitHub userneme?',
     },
 ];
 
-
-// create a function to ask the questions
-
 // Create a function to write README file
 
 // Create a function to initialize app
-
+function init () {
+    inquirer
+        // Prompt the user
+        .prompt ([...questions])
+        // then pass the rosponses to the generateMarkdown function 
+        .then((responses) => {
+            fs.writeFile(
+                generateMarkdown(responses), 
+                'utf8',
+                (err) => {
+                    err ? console.error(err) : console.log("success");
+              })
+        })
+}
 // Function call to initialize app
+init ()
